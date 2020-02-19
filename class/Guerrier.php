8cd2fa4910498fb6de3e7f7b5b7520b36c1b8f79
Melanie <gimmelli.melanie@gmail.com>
2020-02-19 10:35:49 +0100

    GETTERS/SETTER de la class Guerrier + Methode spécifique

---
+++ class/Guerrier.php
@@ -0,0 +1,59 @@
+<?php
+include('Personnage.php');
+class Guerrier extends Personnage
+{
+   private $muscle;
+   //-------------------------------------------//
+   ////////////CONSTRUCTEUR//////////////////////
+   //-----------------------------------------//
+
+
+   public function __construct($nom, $force, $santé, $muscle)
+   {
+      parent::__construct($nom, $force, $santé);
+      $this->setMuscle($muscle);
+   }
+   //-------------------------------------------//
+   ////////////GETTERS//////////////
+   //-----------------------------------------//
+
+   /**
+    *  return $muscle
+    * type : string
+    */
+   public function getMuscle()
+   {
+      $this->muscle;
+   }
+
+
+   //-------------------------------------------//
+   ////////////SETTERS//////////////
+   //-----------------------------------------//
+
+   /**
+    * return $muscle
+    * type :string
+    */
+   public function setMuscle(string $muscle)
+   {
+      $this->muscle = $muscle;
+   }
+
+   //-------------------------------------------//
+   ////////////METHODES SPECIFIQUES//////////////
+   //-----------------------------------------//
+
+   public function seBattre(Guerrier $guerrier)
+   {
+      echo "Je suis " . $guerrier->getNom() . " et je me bats";
+   }
+}
+
+$hulk = new Guerrier("hulk", 50, 100, "beaucoup");
+
+echo $hulk->getNom();
+echo $hulk->getForce();
+echo $hulk->getSanté();
+echo $hulk->getMuscle();
+$hulk->seBattre($hulk);
