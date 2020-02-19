a6328771cfb3c69877c96147e61d7cd2e2893e39
Melanie <gimmelli.melanie@gmail.com>
2020-02-19 09:38:47 +0100

    Initial commit / creation class personnage et debut getters

---
+++ class/Personnage.php
@@ -0,0 +1,31 @@
+<?php
+
+class Personnage
+{
+   private $nom;
+   private $force;
+   private $santé;
+
+   //------------------------//
+   ////////////GETTERS//////////////
+   //------------------------//
+
+   /**
+    * return $nom
+    * type : string
+    */
+   public function getNom()
+   {
+      return $this->nom;
+   }
+
+
+   public function seDeplacer()
+   {
+   }
+
+
+   public  function parler()
+   {
+   }
+}
